5482f4276303eb05053e2a31a8cdea562e4f5c89
Tejas <tejaspant55@gmail.com>
2017-12-26 15:54:21 +0545

    Sentence capitalized

---
+++ q3.py
@@ -0,0 +1,5 @@
+#a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized
+
+
+word = str(input("Enter the string: "))
+print("Given string in capitalized form is '{:s}'.".format(word.upper()))
