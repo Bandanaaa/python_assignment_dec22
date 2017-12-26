e6a2fb6e476594a35edb1f2f95b1e089a4155a02
Tejas <tejaspant55@gmail.com>
2017-12-26 15:52:17 +0545

    Sorted alphabetically

---
+++ q2.py
@@ -0,0 +1,8 @@
+#to sort alphabetically of words separated by comma.
+
+word = str(input("Enter the word separated by comma : "))
+
+sorted_word = sorted(word.split(','))
+final_word = ",".join(sorted_word)
+
+print(final_word)
